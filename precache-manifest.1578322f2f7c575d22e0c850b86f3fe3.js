bfcac3f0ca3844f17e1ce277633f78129575fe3c
ChoMPi <chompibg@gmail.com>
2022-02-08 09:36:10 +0200

    Initial

---
+++ precache-manifest.1578322f2f7c575d22e0c850b86f3fe3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8acfdfd92336f4a507e",
+    "url": "/static/css/main.2d6d7da6.chunk.css"
+  },
+  {
+    "revision": "f8acfdfd92336f4a507e",
+    "url": "/static/js/main.d34a4f1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb02fa35e00c15a4bd9a",
+    "url": "/static/css/2.7b2dd8a1.chunk.css"
+  },
+  {
+    "revision": "eb02fa35e00c15a4bd9a",
+    "url": "/static/js/2.20a89c98.chunk.js"
+  },
+  {
+    "revision": "fd9fbaf211b934adfaa95ca3bfe565df",
+    "url": "/static/media/LapsusPro-Bold.fd9fbaf2.otf"
+  },
+  {
+    "revision": "2a49d8423f736e364b6c09ca942d35c3",
+    "url": "/static/media/background_clouds.2a49d842.png"
+  },
+  {
+    "revision": "a400a47f9dcece90b0d35eca37a5020c",
+    "url": "/static/media/rainbow.a400a47f.png"
+  },
+  {
+    "revision": "277b4344ea6038e04236cebbc8265957",
+    "url": "/index.html"
+  }
+];
